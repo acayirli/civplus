b9f594cdbe2c1f436315c45a6a284a726449a03d
acayirli <a.cayirli@outlook.de>
2022-11-27 20:49:42 +0100

    deleted old components

--- src/componentsOld/radix/logo/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import logo from "../../assets/civ_logo.webp";
-
-export function Logo() {
-    return (
-        <div className="logo">
-            <img src={logo}></img>
-        </div>
-    );
-}
